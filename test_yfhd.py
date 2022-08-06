f56eb150ad68ee76399f21ee76d8d8f4435425ba
Luiz Antonio Calliari Filho <luiz.calliari@sensedata.com.br>
2022-08-06 02:17:11 -0300

    Fixing typing and indentations.

--- test_yfhd.py
+++ test_yfhd.py
@@ -2,6 +2,7 @@ import pytest
 from yahoodata import YahooData
 import pandas
 
+
 @pytest.fixture
 def yfhd_apple_stock():
     return YahooData('AAPL', '13/12/1980', '07/08/2021', '1d')
@@ -10,24 +11,30 @@ def yfhd_apple_stock():
 def test_history_connection(yfhd_apple_stock):
     assert yfhd_apple_stock.show_history_status() == 200
 
+
 def test_div_connection(yfhd_apple_stock):
     assert yfhd_apple_stock.show_div_status() == 200
+
 
 def test_split_connection(yfhd_apple_stock):
     assert yfhd_apple_stock.show_split_status() == 200
 
+
 def test_period1_conversion(yfhd_apple_stock):
     assert yfhd_apple_stock.period1 == '345513600'
+
 
 def test_period1_conversion(yfhd_apple_stock):
     assert yfhd_apple_stock.period2 == '1628294400'
 
+
 def test_history_type_output(yfhd_apple_stock):
-    assert type(yfhd_apple_stock.show_history_data()) ==  pandas.core.frame.DataFrame
+    assert type(yfhd_apple_stock.show_history_data()) == pandas.core.frame.DataFrame
+
 
 def test_div_type_output(yfhd_apple_stock):
-    assert type(yfhd_apple_stock.show_div_data()) ==  pandas.core.frame.DataFrame
+    assert type(yfhd_apple_stock.show_div_data()) == pandas.core.frame.DataFrame
+
 
 def test_split_type_output(yfhd_apple_stock):
-    assert type(yfhd_apple_stock.show_split_data()) ==  pandas.core.frame.DataFrame
-
+    assert type(yfhd_apple_stock.show_split_data()) == pandas.core.frame.DataFrame
