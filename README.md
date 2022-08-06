aac748e728774ebd3634db5302e8c133197e9202
Luiz Antonio Calliari Filho <luiz.calliari@sensedata.com.br>
2022-08-06 14:39:37 -0300

    Functions improvements and readme update.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Yahoo-Finance-Historical-Data
 
-The objective of this project is to facilitate the access to the historical information (stock price, dividend and split) from Yahoo Finance website and show them in a Pandas Dataframe.
+## Instaling
+    python setup.py install
+
+## Requirements
+* Python >= 3.8
+
+## Description
+The objective of this project is to facilitate the access to the historical information (stock price, dividend and 
+split) from Yahoo Finance website and show them in a Pandas Dataframe.
 
 1 - How To Create a object:
-    test = yfhd('AAPL', '13/12/1980', '07/08/2021', '1d')
-    where:
+    <code>test = yfhd('AAPL', '13/12/1980', '07/08/2021', '1d')</code>
 
     'AAPL' is the ticker;
 
@@ -22,13 +29,10 @@ The objective of this project is to facilitate the access to the historical info
 
     test.show_split_data()
 
-
-3 - Getting the status of conection:
+3 - Getting the status of connection (<a href="https://httpstatuses.com/">All the response status</a>)
+    
     test.show_history_status()
 
     test.show_div_status()
 
     test.show_split_status()
-
-    **All the status: https://httpstatuses.com/
-
--- yfhd/yahoodata.py
+++ yfhd/yahoodata.py
@@ -3,73 +3,79 @@ import numpy as np
 import requests
 
 from datetime import datetime
+from typing import Optional, Union
 
 
 class YahooData:
     def __init__(self, ticker: str, period1: str, period2: str, interval: str):
-        self.url = None
+        self.url: Optional[str] = None
         self.ticker: str = ticker
-        self.period1: str = self.convert_date(period1)
-        self.period2: str = self.convert_date(period2)
-        self.interval: str = self.validate_interval(interval)
+        self.period1: str = self._convert_date(date_value=period1)
+        self.period2: str = self._convert_date(date_value=period2)
+        self.interval: str = self._validate_interval(interval=interval)
         self.headers: dict = {'User-Agent': 'None'}
-        self.history_data: requests.models.Responses = self.get_data('history')
-        self.dividend_data: requests.models.Responses = self.get_data('div')
-        self.split_data: requests.models.Responses = self.get_data('split')
+        self.history_data: requests = self._get_data(events='history')
+        self.dividend_data: requests = self._get_data(events='div')
+        self.split_data: requests = self._get_data(events='split')
 
-    def str_to_dataframe(self, string_var: str) -> pd.DataFrame:
-        string_var = string_var.split('\n')
-        if len(string_var) > 1:
-            for aux in range(len(string_var)):
-                string_var[aux] = string_var[aux].split(',')
-            df_string_var = pd.DataFrame(np.array(string_var[1:]), columns=string_var[0])
+    def _str_to_dataframe(self, string_var: str) -> Union[pd.DataFrame, str]:
+        if string_var:
+            string_var = string_var.split('\n')
+            string_aux = []
+            for aux in string_var:
+                string_aux.append(aux.split(','))
+            df_string_var = pd.DataFrame(np.array(string_aux[1:]), columns=string_aux[0])
             df_string_var['Date'] = pd.to_datetime(df_string_var['Date'], format='%Y-%m-%d', errors='coerce')
         else:
             df_string_var = 'empty'
+
         return df_string_var
 
     def show_split_data(self) -> pd.DataFrame:
-        return self.str_to_dataframe(self.split_data.text)
+        return self._str_to_dataframe(string_var=self.split_data.text)
 
     def show_split_status(self) -> int:
         return self.split_data.status_code
 
     def show_div_data(self) -> pd.DataFrame:
-        return self.str_to_dataframe(self.dividend_data.text)
+        return self._str_to_dataframe(string_var=self.dividend_data.text)
 
     def show_div_status(self) -> int:
         return self.dividend_data.status_code
 
     def show_history_data(self) -> pd.DataFrame:
-        return self.str_to_dataframe(self.history_data.text)
+        return self._str_to_dataframe(string_var=self.history_data.text)
 
     def show_history_status(self) -> int:
         return self.history_data.status_code
 
-    def get_data(self, events: str) -> requests.models.Response:
-        self.url = 'https://query1.finance.yahoo.com/v7/finance/download/' + \
-                   self.ticker + '?period1=' + self.period1 + '&period2=' + \
-                   self.period2 + '&interval=' + self.interval + '&events=' \
-                   + events + '&includeAdjustedClose=true'
+    def _get_data(self, events: str) -> requests:
+        self.url = (
+            'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}'
+            '&interval={interval}&events={events}&includeAdjustedClose=true'
+        ).format(ticker=self.ticker, period1=self.period1, period2=self.period2, interval=self.interval, events=events)
+
         return requests.get(self.url, headers=self.headers)
 
-    def convert_date(self, date_value: str) -> str:
+    def _convert_date(self, date_value: str) -> str:
         try:
             date_value_aux = datetime.strptime(date_value, '%d/%m/%Y')
+        except Exception as e:
+            raise ValueError(f'Problem with {date_value}')
+        else:
             date_reference = datetime.strptime('02/01/1962', '%d/%m/%Y')
             if date_reference > date_value_aux:
                 raise ValueError('The minimum date allowed is 02/01/1962')
             else:
                 num_days = date_value_aux - date_reference
-                return str(-252374400 + (num_days.days * 24 * 60 * 60))
-        except:
-            raise ValueError(f'Problem with {date_value}')
 
-    def validate_interval(self, interval: str) -> str:
+                return str(-252374400 + (num_days.days * 24 * 60 * 60))
+
+    def _validate_interval(self, interval: str) -> str:
         if interval in ['1d', '1wk', '1mo']:
             return interval
         else:
-            raise ValueError(f'{interval} Interval must be equal to 1d, 1wk or 1mo')
+            raise ValueError(f'{interval} Interval must be 1d, 1wk or 1mo')
 
 
 if __name__ == '__main__':
